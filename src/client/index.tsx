77c83a475b849ee114ef7c6df5992a2dfe07449e
sago@syun <sago@syun.jp>
2019-05-16 14:28:31 +0900

    router

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import App from './components/App'
 
+function AppRouter() {
+    return (
+        <Router>
+            <Route  exact path="/my/" component={App} />
+        </Router>
+    )
+}
+
 ReactDOM.render(
-    <App />,
+    <AppRouter />,
     document.getElementById('root')
 )
